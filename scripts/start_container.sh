22cbb2cdd1f737bcaae3d0ac4c6d8f85d2704dbd
Aniket Dutta <101723117+4JNxDybbuk@users.noreply.github.com>
2024-10-10 15:17:44 +0530

    add appspec & scripts file

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull aniketnitu2026/node-app-aws-devops
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 aniketnitu2026/node-app-aws-devops
